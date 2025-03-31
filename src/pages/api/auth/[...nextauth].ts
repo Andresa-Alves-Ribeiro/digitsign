020ad5ab33f5a6ed168b67640e647b990cad82d2
Andresa <andresa_15ga@hotmail.com>
2025-03-31 00:05:21 -0300

    fix/resolvendo-erros-da-vercel-4

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -77,7 +77,7 @@ export const authOptions: AuthOptions = {
         error: "/login",
     },
     callbacks: {
-        async jwt({ token, user, account }) {
+        async jwt({ token, user }) {
             if (user) {
                 token.id = (user as CustomUser).id;
             }

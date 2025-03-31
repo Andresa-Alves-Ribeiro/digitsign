5b803bd7d9209c58f1f13838abe06602a81fe1cd
Andresa <andresa_15ga@hotmail.com>
2025-03-31 00:10:01 -0300

    fix/resolvendo-erros-da-vercel-7

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'].filter(ext => !ext.includes('test')),
 };
 
 export default nextConfig;

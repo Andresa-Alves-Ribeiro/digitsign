8bf2c50a993d5d778e912157147c290fe76e4fb3
Andresa <andresa_15ga@hotmail.com>
2025-03-31 00:12:03 -0300

    fix/resolvendo-erros-da-vercel-8

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'].filter(ext => !ext.includes('test')),
+  // Exclude test files from being processed during build
+  webpack: (config, { isServer }) => {
+    // Exclude test files from being processed
+    config.module.rules.push({
+      test: /\.(test|spec)\.(ts|tsx|js|jsx)$/,
+      loader: 'ignore-loader',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
